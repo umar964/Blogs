f0ff21745beba3b719940451db81596d06b67c0b
umar <choudharyumar653@gmail.com>
2025-03-02 22:44:18 +0530

    u

--- Backend/Routes/blogRoutes.js
+++ Backend/Routes/blogRoutes.js
@@ -22,14 +22,10 @@ router.get("/config", (req, res) => {
 // fetch all blogs
 router.get('/', async (req, res) => {
      try{
-    //     const cachedBlogs = await redis.get("all_blogs");
-    //  if(cachedBlogs){
-    //     console.log("Serving blogs from cache");
-    //     return res.json(JSON.parse(cachedBlogs));
-    // }
+    
     const blogs = await Blog.find().sort({ createdAt: -1 });
 
-    // await redis.set("all_blogs", JSON.stringify(blogs));
+     
     res.json(blogs);
      }catch(error){
         console.error("Error fetching blogs:", error);
