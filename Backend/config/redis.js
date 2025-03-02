067cb58ca4c4729ca3e6274543a448da446bf6a5
umar <choudharyumar653@gmail.com>
2025-03-02 16:32:46 +0530

    updates

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -1,10 +1,10 @@
-import { createClient } from "ioredis";
+import Redis from "ioredis";
 
-const redis = new createClient({
-  url: process.env.UPSTASH_REDIS_URL, // Use the Redis URL from .env
+const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
   tls: { rejectUnauthorized: false }, // Required for Upstash
 });
 
 redis.on("error", (err) => console.error("Redis Error:", err));
 
 export default redis;
+
