9f8759eafe69fc170b3b6664f8f0ecc1b00fb42b
umar <choudharyumar653@gmail.com>
2025-07-07 12:32:04 +0530

    --

--- Backend/Routes/blogRoutes.js
+++ Backend/Routes/blogRoutes.js
@@ -158,7 +158,7 @@ router.post("/generate-blog", async (req, res) => {
   const { title } = req.body;
   
   const DEEPINFRA_URL = "https://api.deepinfra.com/v1/openai/chat/completions";
-  const DEEPINFRA_KEY='287X0dBhcRzcMRT21nV8qtt7mnE3zlTr';
+  const DEEPINFRA_KEY="287X0dBhcRzcMRT21nV8qtt7mnE3zlTr";
   // const DEEPINFRA_KEY = process.env.DEEPINFRA_KEY; // Get from https://deepinfra.com/
   
  
